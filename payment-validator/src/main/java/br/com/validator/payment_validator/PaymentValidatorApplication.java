3773626b44b1f115bd020209b39450647f95cb12
gabrielfonseca-de <gabriel.fonseca.on@gmail.com>
2025-05-28 15:30:53 -0300

    First commit

---
+++ payment-validator/src/main/java/br/com/validator/payment_validator/PaymentValidatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.validator.payment_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentValidatorApplication.class, args);
+	}
+
+}
---
+++ ticket/src/test/java/br/com/api_payment/ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_payment.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
